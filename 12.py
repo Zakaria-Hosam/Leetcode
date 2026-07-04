452a02c2e4cf2664acd782cad6f310602a346fb6
Zakaria-Hosam <zakariahosam2004@gmail.com>
2026-07-05 01:06:03 +0300

    cleaner_code

--- 12.py
+++ 12.py
@@ -1,23 +1,10 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        def checks(num):
-            multiplies_of_thousand = num // 1000
-            multiplies_of_hundred = (num % 1000) // 100
-            multiplies_of_ten = (num % 100) // 10
-            multiplies_of_one = num % 10
-            return multiplies_of_thousand, multiplies_of_hundred, multiplies_of_ten, multiplies_of_one
-        multiplies_of_thousand, multiplies_of_hundred, multiplies_of_ten, multiplies_of_one = checks(num)
-        roman_numerals = {
-            1: 'I',
-            5: 'V',
-            10: 'X',
-            50: 'L',
-            100: 'C',
-            500: 'D',
-            1000: 'M'
-        }
-        multiplies_of_thousand, multiplies_of_hundred, multiplies_of_ten, multiplies_of_one = checks(num)
-        def get_roman_numerals(multiplies_of_thousand, multiplies_of_hundred, multiplies_of_ten, multiplies_of_one):
+        def get_roman_numerals(num):
+            thousands = num // 1000
+            hundreds = (num % 1000) // 100
+            tens = (num % 100) // 10
+            ones = num % 10
             roman_numerals = {
                 1: 'I',
                 5: 'V',
@@ -28,41 +15,41 @@ class Solution:
                 1000: 'M'
             }
             roman_string = ''
-            if multiplies_of_thousand > 0 and multiplies_of_thousand < 4:
-                roman_string += roman_numerals[1000] * multiplies_of_thousand
+            if thousands > 0 and thousands < 4:
+                roman_string += roman_numerals[1000] * thousands
             #multiplies of hundred
-            if multiplies_of_hundred > 0 and multiplies_of_hundred < 4:
-                roman_string += roman_numerals[100] * multiplies_of_hundred
-            elif multiplies_of_hundred == 4:
+            if hundreds > 0 and hundreds < 4:
+                roman_string += roman_numerals[100] * hundreds
+            elif hundreds == 4:
                 roman_string += 'CD'
-            elif multiplies_of_hundred == 5:
+            elif hundreds == 5:
                 roman_string += 'D'
-            elif multiplies_of_hundred > 5 and multiplies_of_hundred < 9:
-                roman_string += 'D' + roman_numerals[100] * (multiplies_of_hundred - 5)
-            elif multiplies_of_hundred == 9:
+            elif hundreds > 5 and hundreds < 9:
+                roman_string += 'D' + roman_numerals[100] * (hundreds - 5)
+            elif hundreds == 9:
                 roman_string += 'CM'
             #multiplies of ten
-            if multiplies_of_ten > 0 and multiplies_of_ten < 4:
-                roman_string += roman_numerals[10] * multiplies_of_ten
-            elif multiplies_of_ten == 4:
+            if tens > 0 and tens < 4:
+                roman_string += roman_numerals[10] * tens
+            elif tens == 4:
                 roman_string += 'XL'
-            elif multiplies_of_ten == 5:
+            elif tens == 5:
                 roman_string += 'L'
-            elif multiplies_of_ten > 5 and multiplies_of_ten < 9:
-                roman_string += 'L' + roman_numerals[10] * (multiplies_of_ten - 5)
-            elif multiplies_of_ten == 9:
+            elif tens > 5 and tens < 9:
+                roman_string += 'L' + roman_numerals[10] * (tens - 5)
+            elif tens == 9:
                 roman_string += 'XC'
             #Multiplies of one
-            if multiplies_of_one > 0 and multiplies_of_one < 4:
-                roman_string += roman_numerals[1] * multiplies_of_one
-            elif multiplies_of_one == 4:
+            if ones > 0 and ones < 4:
+                roman_string += roman_numerals[1] * ones
+            elif ones == 4:
                 roman_string += 'IV'
-            elif multiplies_of_one == 5:
+            elif ones == 5:
                 roman_string += 'V'
-            elif multiplies_of_one > 5 and multiplies_of_one < 9:
-                roman_string += 'V' + roman_numerals[1] * (multiplies_of_one - 5)
-            elif multiplies_of_one == 9:
+            elif ones > 5 and ones < 9:
+                roman_string += 'V' + roman_numerals[1] * (ones - 5)
+            elif ones == 9:
                 roman_string += 'IX'
             return roman_string
-        roman_string = get_roman_numerals(multiplies_of_thousand, multiplies_of_hundred, multiplies_of_ten, multiplies_of_one)
+        roman_string = get_roman_numerals(num)
         return roman_string
